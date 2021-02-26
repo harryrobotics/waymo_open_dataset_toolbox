2884e585939c974c8808dfe9f7407b7500921d3d
harry <duongnavy@gmail.com>
2021-02-26 15:21:34 -0500

    modify waymo_utils

--- waymo_utils.py
+++ waymo_utils.py
@@ -1,3 +1,9 @@
+#Some code is written by myself
+#Some code is written by Google Waymo Open dataset team
+#https://github.com/waymo-research/waymo-open-dataset
+#Some code is written by teams from MMLab (Github OpenPCDet)
+# https://github.com/open-mmlab/OpenPCDet
+
 import copy
 import numpy as np
 
@@ -13,43 +19,6 @@ from waymo_open_dataset.utils import transform_utils
 from waymo_open_dataset.utils import  frame_utils
 from waymo_open_dataset import dataset_pb2 as open_dataset
 
-
-def create_simplified_infos(infos,class_names):
-
-    gt_annos = []
-    for info in infos:
-        #metadata
-        gt_lidar_sequence = info['point_cloud']['lidar_sequence']
-        gt_frame_id = info['frame_id']
-        gt_sample_index = info['point_cloud']['sample_idx']
-
-        gt_names = info['annos']['name']
-
-        labels = []
-        for name in gt_names:
-            index = class_names.index(name)
-            labels.append(index)
-        labels = np.asarray(labels,dtype='int')
-
-        gt_boxes = info['annos']['gt_boxes_lidar']
-
-        object_id = info['annos']['obj_ids']
-
-
-        gt_annos.append({
-            "name":
-            gt_names,
-            "gt_labels":
-            labels,
-            "3d_boxes":
-            gt_boxes,
-            "object_id":
-            object_id,
-            "metadata":{"lidar_sequence": gt_lidar_sequence,
-                        "frame_id": gt_frame_id,
-                        "sample_idx": gt_sample_index}
-        })
-    return gt_annos
 
 def print_frame_info(frame):
     print("Location: ",frame.context.stats.location)
@@ -95,7 +64,6 @@ def point_cloud_from_frame(frame):
 
 
     # 3d points in vehicle frame.
-    #0 top 1 front 2 left 3 right 4 rear
     # points_top = points[0]
     # points_front = points[1]
     # points_right = points[3]
@@ -263,181 +231,7 @@ def render_camera(frame,fig_size=(50,40),cam_name='all'):
         cam = cam_list.index(cam_name)
         plt.figure(figsize=fig_size)
         show_camera_image(frame,frame.images[cam], frame.camera_labels, [1, 1, 1])
-        plt.show()        
-#Search
-
-def search_sequence_from_info(sequence_name,infos):
-    '''
-    Args: sequence_name
-    infos dict
-    Output: Start end index of sequence in info dict
-    '''
-    start_index = None
-    stop_index = None
-    for index,info in enumerate(infos):
-        # print(info['point_cloud']['lidar_sequence'])
-        # print(sequence_name)
-        if info['point_cloud']['lidar_sequence'] == sequence_name:
-            if start_index == None:
-                start_index = index
-    stop_index = index
-    if start_index == None:
-        return None, None
-    return start_index,stop_index
-
-
-
-def get_frame_from_index(segment,frame_index):
-    for index,data in enumerate(segment):
-        if index ==frame_index:
-            frame = open_dataset.Frame()
-            frame.ParseFromString(bytearray(data.numpy()))
-            return frame
-    return None
-
-
-#Filter functions
-
-def filter_annos_class(annos, used_class):
-    new_annos = []
-    for anno in annos:
-        filtered_annotations = {}
-        relevant_annotation_indices = [
-            i for i, x in enumerate(anno['name']) if x in used_class
-        ]
-#         print(relevant_annotation_indices)
-        for key in anno.keys():
-            if (key in ['metadata','frame_id','token']):
-                filtered_annotations[key] = anno[key]
-            else:
-                filtered_annotations[key] = (
-                    anno[key][relevant_annotation_indices])
-        new_annos.append(filtered_annotations)
-    return new_annos
-
-
-def filter_annos_front_vehicle(annos,theshold = 0.4):
-    new_annos = []
-    for anno in annos:
-        filtered_annotations = {}
-        relevant_annotation_indices = [i for i, x in enumerate(anno['rotation'])
-                                        if ( (anno['relative_angle'][i] - x) > - theshold
-                                        and (anno['relative_angle'][i] - x) < theshold)]
-        for key in anno.keys():
-            if (key in ['metadata','frame_id','token']):
-                filtered_annotations[key] = anno[key]
-            else:
-                filtered_annotations[key] = (
-                    anno[key][relevant_annotation_indices])
-        new_annos.append(filtered_annotations)
-    return new_annos
-
-def filter_annos_by_rotation(annos,angle):
-    new_annos = []
-    for anno in annos:
-        filtered_annotations = {}
-        relevant_annotation_indices = [i for i, x in enumerate(anno['3d_boxes'][:,6]) if ( x < angle )]
-        for key in anno.keys():
-            if (key in ['metadata','frame_id','token']):
-                filtered_annotations[key] = anno[key]
-            else:
-                filtered_annotations[key] = (anno[key][relevant_annotation_indices])
-        new_annos.append(filtered_annotations)
-    return new_annos
-
-def filter_true_positive(annos,selected_list):
-    new_image_annos = []
-    if isinstance(annos, dict):
-        img_filtered_annotations = {}
-        relevant_annotation_indices = selected_list
-#         print(relevant_annotation_indices)
-#         print(annos)
-        for key in annos.keys():
-            if (key in ['metadata','frame_id','token']):
-                img_filtered_annotations[key] = annos[key]
-            else:
-#                 print(key)
-                img_filtered_annotations[key] = (
-                    annos[key][relevant_annotation_indices])
-        new_image_annos.append(img_filtered_annotations)
-
-    else:
-        for j, anno in enumerate(annos):
-#             print(j)
-            img_filtered_annotations = {}
-            relevant_annotation_indices = selected_list[j]
-#             print(relevant_annotation_indices)
-#             print(anno)
-            for key in anno.keys():
-                if (key in ['metadata','frame_id','token']):
-                    img_filtered_annotations[key] = anno[key]
-                else:
-
-                    img_filtered_annotations[key] = (
-                        anno[key][relevant_annotation_indices])
-            new_image_annos.append(img_filtered_annotations)
-    return new_image_annos
-
-
-
-def distance(anno):
-    new_anno =anno.copy()
-    new_anno['distance'] = np.array([])
-    for i,pos in enumerate(new_anno['3d_boxes']):
-        distance = np.linalg.norm(pos[0:3])
-        new_anno['distance']=np.append(new_anno['distance'],distance)
-    return new_anno
-
-def relative_angle(anno):
-    new_anno = anno.copy()
-    new_anno['relative_angle'] = np.array([])
-    for i,pos in enumerate(new_anno['3d_boxes']):
-        angle = np.arctan2(pos[1],pos[0]) #return angle from 0 to pi or 0 to -pi
-        new_anno['relative_angle']=np.append(new_anno['relative_angle'],angle)
-    return new_anno
-
-def rotation_correction(anno): #convert the rotation y to the same coordinate with relative angle
-    new_anno = anno.copy()
-    new_anno['rotation'] = np.array([])
-    for i,angle in enumerate(new_anno['3d_boxes'][:,6]):
-        # angle = -angle - np.pi/2
-#         if (angle > np.pi):
-#             angle = angle - np.pi
-#         if (angle < -np.pi):
-#             angle = np.pi + angle
-        new_anno['rotation'] = np.append(new_anno['rotation'],angle)
-    return new_anno
-
-
-def add_distance(annos):
-    distance_anno = annos.copy()
-    for i in range(len(annos)):
-        distance_anno[i] = distance(annos[i])
-    return distance_anno
-
-def add_relative_angle(annos):
-    angle_anno = annos.copy()
-    for i in range(len(annos)):
-        angle_anno[i] = relative_angle(annos[i])
-    return angle_anno
-
-def add_rotation_correction(annos):
-    angle_anno = annos.copy()
-    for i in range(len(annos)):
-        angle_anno[i] = rotation_correction(annos[i])
-    return angle_anno
-
-
-
-
-
-
-
-
-
-
-
-
+        plt.show()
 
 
 #Visualisation
