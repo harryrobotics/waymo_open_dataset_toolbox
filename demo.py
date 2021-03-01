41c750096ab1fd7e0e5eb3022b10ac83537fd91e
harry <duongnavy@gmail.com>
2021-03-01 11:43:08 -0500

    modify demo.py to pass data path as argument

--- demo.py
+++ demo.py
@@ -1,17 +1,24 @@
-import numpy as np
-import pickle
 import visualiser as vis
 import waymo_utils as utils
-from pathlib import Path
-from os import walk
+
+import argparse
 
 
-Waymo = utils.Waymo_data('/media/harry/hdd/datasets/waymo')
+if __name__ == '__main__':
 
-sequence_idx = 200
-frame_idx = 100 #Each sequence has about 200 frames
-point_cloud = Waymo.get_lidar(sequence_idx,frame_idx)
-frame = Waymo.get_frame(sequence_idx,frame_idx)
-labels = Waymo.get_lidar_labels(frame)
+    parser = argparse.ArgumentParser(description='arg parser')
+    parser.add_argument('--data_path', type=str, default='/media/harry/hdd/datasets/waymo', help='specify the path  of dataset')
+    args = parser.parse_args()
 
-vis.vis_points_with_multi_boxes(labels['gt_boxes_lidar'],point_cloud)
+    Waymo = utils.Waymo_data(args.data_path)
+
+    assert len(Waymo) != 0, ("No data")
+
+
+    sequence_idx = 200
+    frame_idx = 100 #Each sequence has about 200 frames
+    point_cloud = Waymo.get_lidar(sequence_idx,frame_idx)
+    frame = Waymo.get_frame(sequence_idx,frame_idx)
+    labels = Waymo.get_lidar_labels(frame)
+
+    vis.vis_points_with_multi_boxes(labels['gt_boxes_lidar'],point_cloud)
